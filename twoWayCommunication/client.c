2b00fb3c8c73e63921780ca784e48070e752929e
sumi <sumitwani310@gmail.com>
2026-09-22 13:08:44 +0530

    two way

---
+++ twoWayCommunication/client.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <WinSock2.h>
+#include <string.h>
+
+#pragma cooment(lib, "win_32.lib")
+
+int  main(){
+        WSADATA wsa;
+        SOCKET server_socket, client_socket;
+
+        if()
+}
